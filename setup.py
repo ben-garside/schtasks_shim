c57b8da65b02f791fbd563d804188da2367ebd33
Ben Garside <Ben.Garside@bsigroup.com>
2016-08-16 14:22:46 +0100

    adding 0.1.0

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+SCHTASKS_Shim
+-----------
+python SCHTASKS_Shim
+Link
+`````
+* Source
+  https://github.com/ben-garside/
+"""
+from distutils.core import setup
+
+version = "0.1.0"
+
+setup(
+    name="schtasks_shim",
+    version=version,
+    author="Benjamin Garside",
+    author_email="abgarside<at>gmail<dot>com",
+    packages=[
+        "schtasks_shim"
+        ],
+    include_package_data=True,
+    url="http://github.com/ben-garside/schtasks_shim/dist/{}/".format(version),
+
+    # license="LICENSE.txt",
+    description="schtasks_shim",
+    # long_description=open("README.txt").read() or just """ lots of text here too""",
+    # Dependent packages (distributions)
+    install_requires=[
+    ],
+)
